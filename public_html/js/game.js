b9689894834d05843df06976da18760009387866
Aaron Peron <aaron.peron@yahoo.com>
2015-05-15 14:02:30 -0700

    My map completely vanish on my computer.

--- public_html/js/game.js
+++ public_html/js/game.js
@@ -13,7 +13,7 @@ var game = {
 	// Run on page load.
 	"onload" : function () {
 	// Initialize the video.
-	if (!me.video.init("screen",  me.video.CANVAS, 480, 320, true, 'auto')) {
+	if (!me.video.init("screen",  me.video.CANVAS, 1067, 600, true, 1.0)) {
 		alert("Your browser does not support HTML5 canvas.");
 		return;
 	}
@@ -40,13 +40,8 @@ var game = {
 
 	// Run on game resources loaded.
 	"loaded" : function () {
-                me.pool.register("mario", game.PlayerEntity, true);
-                me.pool.register("levelTrigger", game.LevelTrigger);
-                
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
-                //me.state.set(me.state.LOAD, new game.LoadProfile());
-                //me.state.set(me.state.NEW, new game.NewProfile());
 
 		// Start the game.
 		me.state.change(me.state.PLAY);
--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -5,8 +5,8 @@ game.resources = [
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
-        {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
-        {name: "mario", type:"image", src: "data/img/player1.png"},
+        //{name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+        //{name: "mario", type:"image", src: "data/img/player1.png"},
 
 	/* Atlases 
 	 * @example
